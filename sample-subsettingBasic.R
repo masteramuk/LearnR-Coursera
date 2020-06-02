78dd3539e0881914fe6afc3a0ed7951764136be2
masteramuk <masteramuk@yahoo.com>
2020-06-02 09:23:34 +0800

    update the sample-subsettingBasic with example of subsetting in list

--- sample-subsettingBasic.R
+++ sample-subsettingBasic.R
@@ -7,3 +7,13 @@ x ##similar to print(x)
 x[1] ##print all row of the first col
 x[[2]] ##print all elements of the first object in the second col 
 x$b ##similar as x[[]]
+
+## subsetting lists
+x <- list(a = 1:10, b = 1:5)
+x[1] 
+## will print
+## $a
+## [1]  1  2  3  4  5  6  7  8  9 10
+x[[1]] ## will print similar as x[1] but without header name
+x$b ## will print all elements of the first objects in column b of x
+## x$b = x[["b]] = $b ... only applicable for lists
